5b9a7194babfe10671e5851c59e2bd68ff5e0015
Alexis Torres <axeltux@gmail.com>
2020-05-09 02:49:20 -0500

    Cambios en BuyerScope

--- app/Scopes/BuyerScope.php
+++ app/Scopes/BuyerScope.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Scope;
 use Illuminate\Database\Eloquent\Builder;
 
 class BuyerScope implements Scope
-{
+{    
     public function apply(Builder $builder, Model $model)
     {
         $builder->has('transactions');
